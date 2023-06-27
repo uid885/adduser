da12524116007547c4687e44c6d2656e2d4a0458
christo <135722741+uid885@users.noreply.github.com>
2023-06-27 09:48:13 +0200

    Create adduser.sh

---
+++ adduser.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -
+##########################################################
+# Author:      Christo Deale                  
+# Date  :      2023-06-27                
+# adduser:     Utility to add username & password
+##########################################################
+# Check if the script is being run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root."
+   exit 1
+fi
+
+# Prompt for the new username
+read -p "Enter username: " username
+
+# Prompt for the new password
+read -s -p "Enter password: " password
+echo
+
+# Create the user with the specified username and home directory
+useradd -m -s /bin/bash $username
+
+# Set the password for the new user
+echo "$username:$password" | chpasswd
+
+# Output success message
+echo "User $username has been created successfully with password set."
+
+# Set the ownership and permissions of the user's home directory
+chown -R $username:$username /home/$username
+chmod 700 /home/$username
